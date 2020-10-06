e2dfe63043484ac55ba2c475519dcf2da9118156
Shenica Graham <gshenica@gmail.com>
2020-10-06 11:52:59 -0500

    Moving on from stretch tasks to next module.

--- index.js
+++ index.js
@@ -229,37 +229,34 @@ in 2 steps) */
 and returns the number of world cup wins that country has had. 
 
 Hint: Investigate your data to find "team initials"!
-Hint: use `.reduce` */+
+Hint: use `.reduce` */
 
-function getCountryWins(array, initials) {
+        function getCountryWins(array, initials) {
 
-    let countryMatches = [];    
-    let worldCupWins = [];
+            let countryMatches = [];    
+            let worldCupWins = [];
 
-    for(i = 0; i < array.length; i++) {
-        if ((array[i].Stage === "Final") && ((array[i]["Home Team Initials"] || array[i]["Away Team Initials"]) === initials)) {
-            countryMatches.push(array[i]);
+            for(i = 0; i < array.length; i++) {
+                if ((array[i].Stage === "Final") && ((array[i]["Home Team Initials"] || array[i]["Away Team Initials"]) === initials)) {
+                    countryMatches.push(array[i]);
+                };
+            };
+
+            // Next task: determine how many times "initials" wins in the countryMatches dataset.
+            // I'm having trouble figuring out how to identify the team name as equal to the team initials,
+            // in order to determine if the team won the world cup.
         };
-    };
 
-    // Next task: determine how many "initials" wins in the countryMatches dataset.
-};
-
-getCountryWins(fifaData, "GER");
+        getCountryWins(fifaData, "GER");
 
 
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getGoals();
+/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with
+the most goals scored per appearance (average goals for) in the World Cup finals */
 
 
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
+
+/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team 
+with the most goals scored against them per appearance (average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
 
